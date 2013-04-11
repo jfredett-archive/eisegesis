4be2ea7e148712805b1e6d2fd29deced2b0c3c42
Joe Fredette <jfredett@gmail.com>
2013-04-11 09:14:23 -0400

    wip

--- spec/integration/configuration_spec.rb
+++ spec/integration/configuration_spec.rb
@@ -9,6 +9,8 @@ describe Eisegesis::Configuration do
     puts "project_path exists? #{File.directory? project_path}"
     puts "content of project_path: #{`ls #{project_path}`}"
     puts "content of path via Dir: #{Dir[File.join(project_path, '*')]}"
+    puts "content via Exegesis: #{project_directory.content}"
+    puts "config value: #{configuration.config}"
   end
 
   let(:project_path) { File.expand_path(File.join('..', '..', 'fake_project'), __FILE__) }
